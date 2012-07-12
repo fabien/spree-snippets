e112b21881d99250a98bb70a8fa8e221cc7d06fb
Fabien Franzen <info@atelierfabien.be>
2012-07-12 21:31:53 +0200

    Added is_active flag, use standard form layout (removed formtastic dependency)

--- app/models/spree/snippet.rb
+++ app/models/spree/snippet.rb
@@ -1,5 +1,9 @@
 class Spree::Snippet < ActiveRecord::Base
-  validates :slug, :presence => true, :uniqueness => true
+  
+  attr_accessible :slug, :content, :is_active
+  
+  validates :slug, :presence => true, :uniqueness => true, :on => :update
   scope :active, where(:is_active => true)
   scope :localized, lambda { |prefix| where("snippets.slug LIKE ?", "/#{prefix}/%") }
+  
 end
